25003308fdf5d4cf8a0a24011fbb2d3dd5885117
Rain Wei <Rain Wei>
2017-02-03 18:17:03 +0800

    +0005

---
+++ 0005/resize_pics.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  3 17:34:09 2017
+
+@author: dmall
+"""
+
+import os
+from PIL import Image
+
+def resize_pictures(path):
+    """resize all the pictures in path"""
+    os.chdir(path)
+    file_in_current_dir = os.listdir()
+    for i in file_in_current_dir:
+        with Image.open(i) as im:
+            w, h = im.size
+            # 按比例偏大的边来计算整体需要缩小的比例
+            n = w / 1136 if (w / 1136) >= (h / 640) else h / 640
+            im.thumbnail((w / n, h / n))
+            im.save(i.split('.')[0] + '.jpg', 'jpeg')
+
+if __name__ == '__main__':
+    resize_pictures('d:\\learn\\python\\pics')
+
+
+
+
+
+
